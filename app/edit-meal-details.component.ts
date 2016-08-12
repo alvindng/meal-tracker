b65640d068faf4ef264cb3af11ec53ef6c8741d9
Alvin Ng <alvindng@gmail.com>
2016-08-12 14:45:15 -0700

    add ability to edit calorie count for a meal

--- app/edit-meal-details.component.ts
+++ app/edit-meal-details.component.ts
@@ -5,11 +5,13 @@ import { Meal } from './meal.model';
   selector: 'edit-meal-details',
   inputs: ['meal'],
   template: `
-    <div class='meal-form'>
+    <div class='meal-form' col-sm-6>
     <h4>Edit Meal: {{meal.name }}</h4>
-    <input [(ngModel)]="meal.name"/>
+    <input [(ngModel)]="meal.name" class='input-lg'/>
     <h5>Edit Details: {{meal.details }}</h5>
-    <input [(ngModel)]="meal.details"/>
+    <input [(ngModel)]="meal.details" class='input-lg'/>
+    <h5>Edit Calorie Count: {{meal.calories }}</h5>
+    <input [(ngModel)]="meal.calories" class='input-lg'/>
     </div>
   `
 })

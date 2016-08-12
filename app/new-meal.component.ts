2cce0f70a52046781f3475b96d70ef4587fcbb3b
Alvin Ng <alvindng@gmail.com>
2016-08-12 11:21:17 -0700

    add component to add a new meal

---
+++ app/new-meal.component.ts
@@ -0,0 +1,13 @@
+import { Component } from 'angular2/core';
+import { Meal } from './meal.model';
+
+@Component({
+  selector: 'new-meal',
+  template: `
+    <h3>test</h3>
+  `
+})
+
+export class NewMealComponent{
+
+}

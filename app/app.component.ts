84843c87680a0160bf256be57d744c249b2159f9
Alvin Ng <alvindng@gmail.com>
2016-08-12 10:02:26 -0700

    add constuctor for appComponent

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,10 +6,17 @@ import { Meal } from './meal.model';
   template: `
     <div class='container'>
       <h1>Skeleton Angular2 App!</h1>
+      
     </div>
   `
 })
 
 export class AppComponent {
-
+  public meals: Meal[];
+  constructor(){
+    this.meals = [
+    new Meal("Hamburger", "Didn't get a soda or cheese on my burger!", 354, 0),
+    new Meal("Fries", "I only ate half of them.", 365, 1)
+  ];
+  }
 }
